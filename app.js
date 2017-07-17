30c377ee239a6264f4cb728908c060988f19a2e5
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-07-17 13:57:08 +0100

    user model setup

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyparser = require('bodyparser');
-var cors = require('cors');
-var path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const passport = require('passport');
+const config = require('./config/database')
+const app = express();
 
-var app = express();
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+  console.log(`connected to database ${config.database}`);
+})
+
+mongoose.connection.on('connected', () => {
+  console.log(`connected to database ${config.database}`);
+})
+const users = require('./routes/users');
 
 const port = 3000;
+
+app.use(cors());
+
+app.use(bodyparser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users,', users)
 
 app.get('/', (req, res) => {
   res.send('homepage')
 })
+
+
 app.listen(port,() => {
-  console.log(`server started at ${port} 3000`);
+  console.log(`server started at ${port}`);
 });
