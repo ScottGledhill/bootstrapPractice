8f8b431a2f1c3b6d3ea59e3b4a62a27f17496cbe
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-07-17 12:19:39 +0100

    homepage route

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var app = express();
 
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('homepage')
+})
 app.listen(port,() => {
-  console.log(`server started at ${port} 3000`)
-  }
-);
+  console.log(`server started at ${port} 3000`);
+});
